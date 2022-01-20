db59a11b0eda15d33895a42909ef47ca8aa33b03
dhavalkakdiya <dhaval.ldit@gmail.com>
2022-01-20 19:54:31 +0000

    update download_image fun add upload json fun

--- scaper.py
+++ scaper.py
@@ -11,6 +11,11 @@ import os
 from tqdm import tqdm
 import urllib.request
 import uuid
+import json
+import warnings
+
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 class ScrapUrl():
     
@@ -44,7 +49,7 @@ class ScrapUrl():
             cookies_btn.click()
         except:
             print(cookies_btn.text)
-    
+        print("Cookies button accepted")
 
     def search_city(self, city : str, xpath : str)-> None:
 
@@ -62,6 +67,7 @@ class ScrapUrl():
         serch_section.send_keys(Keys.RETURN)
         serch_section.send_keys(city)
         serch_section.send_keys(Keys.RETURN)
+        print(f'{city} enter')
 
     def hotel_tab(self, xpath_hotel_tab: str) -> None:
         """click to hotel tab
@@ -71,8 +77,10 @@ class ScrapUrl():
         """
         time.sleep(3)
         self.driver.find_element_by_xpath(xpath_hotel_tab).click()
+        print("hotel tab clicked")
 
-    def get_urls_of_Hotel(self, xpath_hotel: str)-> list:
+
+    def get_urls_of_Hotel(self,page_n:int, xpath_hotel: str)-> list:
         """Get url of all hotel
 
         Args:
@@ -83,7 +91,7 @@ class ScrapUrl():
         time.sleep(3)
         
         hotel_links =[]
-        for i in range(1,6):
+        for i in range(1,page_n+1):
             
             list_hotels = self.driver.find_elements_by_xpath(xpath_hotel)
             for i in range(len(list_hotels)):
@@ -95,41 +103,41 @@ class ScrapUrl():
             
             len(hotel_links) 
             time.sleep(5)
-        
+        print("hotel links done")
         return hotel_links
 
-    def get_images(self, xpath_hotel: str)-> list:
+    def get_images(self,page_n:int, xpath_img: str)-> list:
         """Get url of all hotel
 
         Args:
             
-            xpath_hotel (str): xpath of hotel '//a[@class = "review_count"]'
+            xpath_img (str): xpath of hotel '//div[@class="prw_rup prw_common_responsive_image"]/div[1]/div'
             
         """
         time.sleep(3)
         
         image_links = []
-        hotel_links =[]
-        for i in range(1,6):
+        
+        for i in range(1,page_n+1):
             
 
             # images  
-            div_img = self.driver.find_elements_by_xpath('//div[@class="prw_rup prw_common_responsive_image"]/div[1]/div')
+            div_img = self.driver.find_elements_by_xpath(xpath_img)
             #imgtags = self.driver.find_elements_by_xpath('//div[@class="inner"]')
             print(len(div_img))
             for j in range(len(div_img)):
                 imgpath = div_img[j].get_attribute("style")
                 image_links.append(imgpath[23:-3])
-                print(imgpath[23:-3])
+                #print(imgpath[23:-3])
             
 
 
             # next page button click
             self.driver.find_element_by_link_text("Next").click()
             
-            len(hotel_links) 
+             
             time.sleep(5)
-        #print(image_links)
+        print("image link done")
         return image_links
 
     
@@ -137,31 +145,32 @@ class ScrapUrl():
         uuid_list = []
         for i in range(len(links)):
             uuid_list.append(uuid.uuid4().hex)
+        print("uuid gen done")
+        return uuid_list
     
-    def download_images(self,image_links:list, path='.') -> None:
+    def download_images(self,city:str,image_links:list, uid_list:list, path='.') -> None:
         '''
         This method will download the images to the specified path
         '''
-        
-        
-        if not os.path.exists(f'{path}/images'):
-            os.makedirs(f'{path}/images')
+        s3_client = boto3.client('s3')
+        id_image_dict = dict(zip(uid_list,image_links))
+        #print(id_image_dict)
+        if not os.path.exists(f'{path}/images/{city}'):
+            os.makedirs(f'{path}/images/{city}')
         if image_links is None:
             print('No images found, plase run get_images() first')
             return None
         
-        for i, scr in enumerate(tqdm(image_links)):
-            urllib.request.urlretrieve(scr, f'{path}/images/hotel_{i}.jpg')
-            
-            
         #for i, scr in enumerate(tqdm(image_links)):
-        #'    urllib.request.urlretrieve(scr, f'{i}.jpg')
-        #for i in image_links:   
-        #    with open('{i}.jpg', 'wb') as file:
-        #      file.write(i.screenshot_as_png)
+        #    urllib.request.urlretrieve(scr, f'{path}/images/hotel_{i}.jpg')
+
+        for key, value in tqdm(id_image_dict.items()):
+            urllib.request.urlretrieve(value, f'{path}/images/{city}/{key}.jpg')
+            s3_client.upload_file(f'{path}/images/{city}/{key}.jpg', 'aicoredata', f'images/{city}/{key}.jpg')
+            time.sleep(2)
+        print("image downloded")
     
-    
-    
+    '''
     def upload_image_aws(self, image_links : list):
         # extracting images
         s3_client = boto3.client('s3')
@@ -171,20 +180,26 @@ class ScrapUrl():
                 urllib.request.urlretrieve(scr, f'{temp_dir}/{i}hotel.jpg')
                 s3_client.upload_file(f'{temp_dir}/{i}hotel.jpg', 'aicoredata', f'{i}hotel.jpg')
                 time.sleep(2)
+    '''
 
-
-    def get_hotel_data(self, hotellist : list)-> dict:
-        """get hotel information, and ameneties
+    def get_hotel_data(self, uidlist:list, hotellist : list)-> dict:
+        """get hotel information,UK220117-59732534 and ameneties
         Args:
             hotellist (list): list of hotels
         """
+       
 
-        hotel_info = {"name":[],"address":[], "reviews":[], "mail_id":[],"rating": [],"amenities": []}
+        hotel_info = {"id":[],"name":[],"address":[], "reviews":[], "mail_id":[],"rating": [],"amenities": []}
+        hotel_info['id'] = uidlist
+
         
-
-        for i in range(len(hotellist)):
-            time.sleep(3)
+        for i in tqdm(range(len(hotellist))):
+            time.sleep(2)
+            
             self.driver.get(hotellist[i])
+            
+            
+            
             try:
                 hotel_name = self.driver.find_element_by_xpath('//h1[@id = "HEADING"]').text
 
@@ -218,11 +233,11 @@ class ScrapUrl():
             '''
 
             try:
-                hotel_mail_id = self.driver.find_element_by_xpath('//a[@class = "bIWzQ fWKZw"]').get_attribute('href')
+                hotel_mail_id = self.hotel_linksdriver.find_element_by_xpath('//a[@class = "bIWzQ fWKZw"]').get_attribute('href')
             except:
                 hotel_mail_id = ""
             hotel_info["mail_id"].append(hotel_mail_id)
-            print(hotel_mail_id)
+            #print(hotel_mail_id)
 
 
             try:
@@ -230,7 +245,7 @@ class ScrapUrl():
             except:
                 hotel_rating = ""
             hotel_info["rating"].append(hotel_rating)
-            print(hotel_rating)
+            #print(hotel_rating)
 
             amenities = self.driver.find_elements_by_xpath('//div[@class="bUmsU f ME H3 _c"]')
             property_amenities = []
@@ -240,36 +255,58 @@ class ScrapUrl():
                     property_amenities.append(amenity)
             hotel_info["amenities"].append(' ,'.join(map(str,list(set(property_amenities)))))
 
-        
+        print("hotel data get done")
         return hotel_info
 
+    def dict_json_s3(self,city:str, hotel_data_dict:dict):
+           
+        
+        s3 = boto3.client('s3')
+            
+        # Serializing json  
+        with open(f'{city}.json', "w") as outfile:
+            json.dump(hotel_data_dict, outfile)
 
+        with open(f'{city}.json', "rb") as f:
+            s3.upload_fileobj(f, "aicoredata", f'{city}.json')
+        print("json file added to s3")
 
 
 
 
 def main():
+    city = input('Enter the name of the city from which you want to scrape hotel data from: ')
+    page = int(input('Enter the number of the pages you want to scrap!'))
     chrome_options = Options()
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument('log-level=3')
     obj = ScrapUrl(chrome_options)
     time.sleep(3)
     obj.web_driver("https://www.tripadvisor.co.uk/")
     time.sleep(3)
     obj.accept_cookies_button('//*[@id="onetrust-accept-btn-handler"]')
     time.sleep(3)
-    obj.search_city('London', '//*[@id="lithium-root"]/main/div[3]/div/div/div[2]/form/input[1]')
+    obj.search_city(city, '//*[@id="lithium-root"]/main/div[3]/div/div/div[2]/form/input[1]')
     time.sleep(3)
     obj.hotel_tab('//*[@id="search-filters"]/ul/li[2]/a')
     time.sleep(3)
-    hotel_links = obj.get_urls_of_Hotel('//a[@class = "review_count"]')
+    hotel_links = obj.get_urls_of_Hotel(page,'//a[@class = "review_count"]')
     time.sleep(3)
-    image_links = obj.get_images('//a[@class = "review_count"]')
+    image_links = obj.get_images(page,'//div[@class="prw_rup prw_common_responsive_image"]/div[1]/div')
     time.sleep(3)
     uuid_list = obj.gen_uuid(image_links)
     time.sleep(3)
-    hotel_data_dict =obj.get_hotel_data(hotel_links)
-    print(hotel_data_dict)
+    
+    obj.download_images(city,image_links, uuid_list)
+    time.sleep(3)
+    hotel_data_dict = obj.get_hotel_data(uuid_list, hotel_links)
+    
+    time.sleep(3)
+    obj.dict_json_s3(city,hotel_data_dict)
+
+
     upload_aws_rds(hotel_data_dict)
+
 
 def upload_aws_rds(hotel_dict:dict):
     df = pd.DataFrame.from_dict(hotel_dict)
