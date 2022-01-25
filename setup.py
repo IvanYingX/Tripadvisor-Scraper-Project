ec7fa51bc61c6870521f0d1450c893f373405577
dhavalkakdiya <dhaval.ldit@gmail.com>
2022-01-25 18:55:11 +0000

    edit setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import find_packages
 
 setup(
     name='Tripadvisor_Scraper_Project',
-    version='0.boto30.10',
+    version='0.0.11',
     description='Mock package that scrap data of hotels from Tripadvisor',
     #url='https://github.com/IvanYingX/Tripadvisor-Scraper-Project',
     author='dhaval, luqman, aicore',
     license='MIT',
     packages=find_packages(),
-    install_requires=['selenium','sqlalchemy','tqdm','pandas','webdriver_manager','boto3',''],
+    
     install_requires=['selenium','sqlalchemy','tqdm','pandas','webdriver_manager','boto3','warnings','requests'],
 
 
